72f0285fccb95214edb3fe559a36534f847e42ff
guillermo eyherabide <ne0guille@hotmail.com>
2019-08-12 13:13:20 -0300

    add key to component to re render on change

--- src/screens/Tracks/Main.js
+++ src/screens/Tracks/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
-
 import { isEmpty } from 'ramda';
+
 import AudioPlayer from "./components/AudioPlayer";
 
 export const Main = ({ track = {}, isPlaying }) => {
@@ -10,7 +10,7 @@ export const Main = ({ track = {}, isPlaying }) => {
       {isEmpty(track) ? (
         <p>Select track to play</p>
       ) : (
-        <AudioPlayer name={name} source={source} isPlaying={isPlaying} />
+        <AudioPlayer key={name} name={name} source={source} isPlaying={isPlaying} />
       )}
     </div>
   );
